4ff2cd11637c88b5425f0c26d399a2914d74520a
Wesley Menezes <contato@wesleymenezes.com.br>
2025-10-28 17:17:28 -0300

    fix: remover TypeORM duplicado do HealthModule
    - HealthModule tinha TypeOrmModule.forRoot() duplicado
    - Isso causava inicialização dupla do TypeORM
    - Removido import e TypeOrmModule do HealthModule
    - TypeORM já é configurado globalmente no AppModule

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
   controllers: [HealthController],
   providers: [HealthService],
 })
